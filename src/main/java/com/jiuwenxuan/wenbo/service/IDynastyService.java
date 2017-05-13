a676a652ff60bba834ee371f371eb9be310b8c4f
zhouchongchong <764098483@qq.com>
2017-05-13 13:14:22 +0800

    some change for upload

--- src/main/java/com/jiuwenxuan/wenbo/service/IDynastyService.java
+++ src/main/java/com/jiuwenxuan/wenbo/service/IDynastyService.java
@@ -1,5 +1,24 @@
-package com.jiuwenxuan.wenbo.service;
-
+/**  
+ * Project Name:jiuwenxuan  
+ * File Name:IDynastyService.java  
+ * Package Name:com.jiuwenxuan.wenbo.service  
+ * Date:2017年5月13日上午10:55:33  
+ * Copyright (c) 2017, 7640*****@qq.com All Rights Reserved.  
+ *  
+*/  
+  
+package com.jiuwenxuan.wenbo.service;  
+/**  
+ * ClassName:IDynastyService <br/>  
+ * Function: TODO ADD FUNCTION. <br/>  
+ * Reason:   TODO ADD REASON. <br/>  
+ * Date:     2017年5月13日 上午10:55:33 <br/>  
+ * @author   zhouchong  
+ * @version    
+ * @since    JDK 1.8  
+ * @see        
+ */
 public interface IDynastyService {
 
 }
+  
